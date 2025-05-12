4180046f5aa5ac690690b83138cc249ff8f1e44b
NefiO13 <526Nokojie@frhsd.com>
2025-05-12 10:38:06 -0400

    first commit

---
+++ player.js
@@ -0,0 +1,41 @@
+const players = [
+    {
+        firstName: 'Stephen',
+        lastName: 'Curry',
+        position: 'Sg',
+        age: '36',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Stephen',
+        lastName: 'Curry',
+        position: 'Sg',
+        age: '36',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Stephen',
+        lastName: 'Curry',
+        position: 'Sg',
+        age: '36',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Stephen',
+        lastName: 'Curry',
+        position: 'Sg',
+        age: '36',
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName: 'Stephen',
+        lastName: 'Curry',
+        position: 'Sg',
+        age: '36',
+        photo: 'https://picsum.photos/200/300'
+    }
+]
