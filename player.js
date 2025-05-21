d5ebc06a20db106ef80140f64f410161d2987610
NefiO13 <526Nokojie@frhsd.com>
2025-05-21 10:28:19 -0400

    editing cards

--- player.js
+++ player.js
@@ -1,41 +1,126 @@
 const players = [
     {
-        firstName: 'Stephen',
-        lastName: 'Curry',
-        position: 'Sg',
-        age: '36',
+        firstName: 'LeBron',
+        lastName: 'James',
+        position: 'SF',
+        age: '40',
+        photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254'
+    },
+
+    {
+        firstName: 'Austin',
+        lastName: 'Reaves',
+        position: 'SG',
+        age: '26',
         photo: 'https://picsum.photos/200/300'
     },
 
     {
-        firstName: 'Stephen',
-        lastName: 'Curry',
-        position: 'Sg',
-        age: '36',
+        firstName: 'Luka',
+        lastName: 'Doncic',
+        position: 'PG',
+        age: '26',
         photo: 'https://picsum.photos/200/300'
     },
 
     {
-        firstName: 'Stephen',
-        lastName: 'Curry',
-        position: 'Sg',
-        age: '36',
+        firstName: 'Dorian',
+        lastName: 'Finney-Smith',
+        position: 'PF',
+        age: '32',
         photo: 'https://picsum.photos/200/300'
     },
 
     {
-        firstName: 'Stephen',
-        lastName: 'Curry',
-        position: 'Sg',
-        age: '36',
+        firstName: 'Jordan',
+        lastName: 'Goodwin',
+        position: 'G',
+        age: '26',
         photo: 'https://picsum.photos/200/300'
     },
 
     {
-        firstName: 'Stephen',
-        lastName: 'Curry',
-        position: 'Sg',
-        age: '36',
+        firstName: 'Rui',
+        lastName: 'Hachimura',
+        position: 'PF',
+        age: '27',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Jaxson',
+        lastName: 'Hayes',
+        position: 'C',
+        age: '24',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Bronny',
+        lastName: 'James',
+        position: 'G',
+        age: '20',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Trey',
+        lastName: 'Jemison III',
+        position: 'C',
+        age: '25',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Maxi',
+        lastName: 'Kleber',
+        position: 'PF',
+        age: '33',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Dalton',
+        lastName: 'Knecht',
+        position: 'G',
+        age: '24',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Christian',
+        lastName: 'Koloko',
+        position: 'C',
+        age: '24',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Alex',
+        lastName: 'Len',
+        position: 'C',
+        age: '31',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Shake',
+        lastName: 'Milton',
+        position: 'SG',
+        age: '28',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Markieff',
+        lastName: 'Morris',
+        position: 'PF',
+        age: '35',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Jarred',
+        lastName: 'Vanderbilt',
+        position: 'PF',
+        age: '26',
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Gabe',
+        lastName: 'Vincent',
+        position: 'PG',
+        age: '26',
         photo: 'https://picsum.photos/200/300'
     }
 ]
